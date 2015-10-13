95c433f90dc7e71c70000fc75e4d709adbd65b79
Jess <jdantasl@gmail.com>
2015-10-13 11:03:43 -0300

    Update ft3_cenario3.rb

--- ft3_cenario3.rb
+++ ft3_cenario3.rb
@@ -1,14 +1,14 @@
-language: pt
-enconding: utf-8
+#language: pt
+#enconding: utf-8
 
-# Dado(/^que eu escolhi uma tv$/) do
-  # visit 'https://www.walmart.com.br/smart-tv-led-full-hd-43-lg-43lf6350-webos-3-usb-3-hdmi-wi-fi-integrado/3088158/pr'  
-# end
+ Dado(/^que eu escolhi uma tv$/) do
+   visit 'https://www.walmart.com.br/smart-tv-led-full-hd-43-lg-43lf6350-webos-3-usb-3-hdmi-wi-fi-integrado/3088158/pr'  
+ end
 
-# Quando(/^eu aciono o botao "Adicionar ao carrinho"$/) do |arg1|
-  # click_button 'Adicionar ao carrinho'  
-# end
+ Quando(/^eu aciono o botao "Adicionar ao carrinho"$/) do |arg1|
+   click_button 'Adicionar ao carrinho'  
+ end
 
-# Entao(/^o item escolhido devera entrar na lista da pagina do carrinho$/) do
-  # expect(page).to have_content 'Meu carrinho (1 item)'  
-# end
+ Entao(/^o item escolhido devera entrar na lista da pagina do carrinho$/) do
+   expect(page).to have_content 'Meu carrinho (1 item)'  
+ end
